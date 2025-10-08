8249311f012548c050a54999e06580d35dbcd415
M4NithinReddy <mnithinreddy0403@gmail.com>
2025-10-08 11:23:35 +0530

    commited

--- index.tsx
+++ index.tsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import App from './src/App';
+import { AuthProvider } from './src/context/AuthContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
